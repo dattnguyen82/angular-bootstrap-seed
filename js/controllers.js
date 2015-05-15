6b4fe3042fc9418b8f4cea9855beda3c0a597b65
Dat Nguyen <dat.nguyen@ge.com>
2015-05-15 00:19:40 -0700

    removing email

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,5 @@
 /* Controllers */
 angular.module('myApp.controllers', [])
    .controller('view1Controller', function ($scope, user, app_cache, nr_services, nr_util, nr_appetite_util) {
-      $scope.text = "View 1 Controller Text";
+      $scope.controller_text = "View 1 Controller Text";
     });
