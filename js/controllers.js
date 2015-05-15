846af98c3b6225035b33d03be1228e8c93491c8e
Dat Nguyen <dat.nguyen@ge.com>
2015-05-15 00:34:42 -0700

    removing unused

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,6 @@
 
 /* Controllers */
 angular.module('myApp.controllers', [])
-   .controller('view1Controller', function ($scope, user, app_cache, nr_services, nr_util, nr_appetite_util) {
+   .controller('view1Controller', function ($scope) {
       $scope.controller_text = "View 1 Controller Text";
     });
